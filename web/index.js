da1b565c971b9e6ecea50a012df34ec18dc7f995
Roseshadows - 离影玫 <99497945+Roseshadows@users.noreply.github.com>
2023-05-22 18:33:41 +0800

    Add files via upload

---
+++ web/index.js
@@ -0,0 +1,85 @@
+$(document).ready(()=> {
+    $("div.advanced-settings div.row").click(function() {
+        $("ul.settings-columns").slideToggle();
+        $("div.advanced-settings div.row i.arrow-down").toggleClass("fa-caret-down");
+        $("div.advanced-settings div.row i.arrow-down").toggleClass("fa-caret-up");
+    });
+    $("span.clear-text").click(()=>{
+        $("div.search-input input.search-area").val("");
+        $("span.clear-text").css("visibility",'hidden');
+    });
+    $("div.search-input input.search-area").on('input',function(){
+        var value = $("div.search-input input.search-area").val();
+        if(value) {
+            $("span.clear-text").css("visibility",'visible');
+        } else {
+            $("span.clear-text").css("visibility",'hidden');
+        }
+    });
+    $("#tags-selection").on('click','span',function(){
+    });
+    $('span.tags-list-btn').click(()=>{
+        Global.getTags((tags)=>{
+            tags = Global.convertTagStruct(tags);
+            var tag_list = Global.getTagsForSelection(['*'], tags);
+            tag_list.shift(); // 去掉数组开头的*字符
+            var temp_str = '';
+            tag_list.forEach((item)=>{
+                var s_a = '', i_a = '';
+                if(Global._search_tags && JSON.stringify(Global._search_tags) != '[]' && Global._search_tags.contains(item)){
+                    s_a = ' selected';
+                    i_a = '-dot';
+                }
+                temp_str += '<span class="selectable-tag'+s_a+'"><i class="fa fa'+i_a+'-circle-o"></i><span>'+item+'</span></span>';
+            });
+            $.confirm({
+                title: '选择TAG',
+                content: '<div id="tags-selection">'+temp_str+'</div>',
+                boxWidth: "80%",
+                useBootstrap: false,
+                theme: 'light',
+                type: 'blue',
+                buttons: {
+                    submit: {
+                        text: '确认',
+                        action: function () {
+                            if(Global.__temp_tags && JSON.stringify(Global.__temp_tags) != '[]') {
+                                // 待改!!!
+                                Global._search_tags = Global._search_tags || [];
+                                Global.__temp_tags = Global.__temp_tags.clearRepetition();
+                                Global._search_tags = Global._search_tags.concat(Global.__temp_tags);
+                                Global.__temp_tags.forEach((item)=>{
+                                    $('span.tags-input-area').append('<span class="shown-tag"><i class="fa fa-tag"></i><span>'+item+'</span></span>')
+                                })
+                            }
+                        }
+                    },
+                    cancel: {
+                        text: '关闭',
+                        action: function () {
+                            // console.log('no')
+                        }
+                    }
+                },
+                onOpen: function(){
+                    // 绑定事件
+                    this.$content.find("span.selectable-tag").click(function(){
+                        var tag = $(this).find('span').text();
+                        if(!Global.__temp_tags || JSON.stringify(Global.__temp_tags) == '[]' || !Global.__temp_tags.contains(tag)) {
+                            Global.__temp_tags = Global.__temp_tags || [];
+                            Global.__temp_tags.push(tag);
+                            $(this).addClass('selected');
+                            $(this).find('i').removeClass('fa-circle-o');
+                            $(this).find('i').addClass('fa-dot-circle-o');
+                        } else if(Global.__temp_tags.contains(tag)) {
+                            Global.__temp_tags.remove(tag);
+                            $(this).removeClass('selected');
+                            $(this).find('i').removeClass('fa-dot-circle-o');
+                            $(this).find('i').addClass('fa-circle-o');
+                        }
+                    })
+                }
+            });
+        })
+    });
+})
--- web/tags-handler.js
+++ web/tags-handler.js
@@ -1,7 +1,7 @@
 var Global = Global || {};
 /**
  * 解析TAG的JSON结构
- * @param {object} obj 传入的JSON数据文件中TAG结构JSON字符串
+ * @param {object} obj TAG原始对象 (JSON数据文件中的对象)
  * @returns 解析好的一个对象。
  */
 Global.convertTagStruct = function(obj) {
@@ -52,10 +52,9 @@ Global.convertTagStruct = function(obj) {
  * 获取用于搜索的TAG数组。
  * @param {array} s 用户端输入的TAG数组
  * @param {object} obj 用 convertTagStruct 方法转换过的对象
- * @param {boolean} isForSelection 是否用于用户端TAG选择功能
  * @returns 按照树状结构生成的用于搜索文章的TAG数组。
  */
-Global.getTagsForSearch = function(s, obj, isForSelection) {
+Global.getTagsForSearch = function(s, obj) {
     var temp_arr = s || null;
     var temp_arr2 = [];
     var t_tags = temp_arr;
@@ -77,15 +76,49 @@ Global.getTagsForSearch = function(s, obj, isForSelection) {
         }
     };
     var target_tags = t_tags.clearRepetition();
-    if(isForSelection) target_tags = this._convertMultiNamedTagsToSingle(target_tags);
     // 注意以下两行代码的顺序
     target_tags = this._seperateMultiNamedTags(target_tags);
     target_tags = this._removeBannedTags(target_tags);
     return target_tags;
 }
 
+/**
+ * 获取用于用户端选择的TAG数组。
+ * @param {array} s 用户端输入的TAG数组
+ * @param {object} obj 用 convertTagStruct 方法转换过的对象
+ * @returns 列在TAG数据文件中用于用户端选择的所有TAG的数组
+ */
+Global.getTagsForSelection = function(s, obj) {
+    var temp_arr = s || null;
+    var temp_arr2 = [];
+    var t_tags = temp_arr;
+    var keys = Object.keys(obj);
+    while(temp_arr && JSON.stringify(temp_arr) != '[]') {
+        temp_arr2 = [];
+        for(var i = 0; i < temp_arr.length; i++) {
+            keys.forEach((item)=>{
+                if(item == temp_arr[i]) {
+                    temp_arr2 = temp_arr2.concat(obj[item]);
+                    t_tags = t_tags.concat(obj[item]);
+                }
+            });
+        }
+        if(JSON.stringify(temp_arr2) != '[]'){
+            temp_arr = temp_arr2;
+        } else {
+            temp_arr = [];
+        }
+    };
+    var target_tags = t_tags.clearRepetition();
+    // 注意以下两行代码的顺序
+    target_tags = this._convertMultiNamedTagsToSingle(target_tags);
+    target_tags = this._removeBannedTags(target_tags);
+    target_tags = target_tags.clearRepetition();
+    return target_tags;
+}
+
 Global._removeBannedTags = function(dirt_list){
-    var banned_tags = Global.getPreference('banned-tags');
+    var banned_tags = Global.getPreference('banned-tags') || [];
     var clean_list = [];
     for(var i = 0; i < dirt_list.length; i++){
         var tag = dirt_list[i];
@@ -112,8 +145,55 @@ Global._seperateMultiNamedTags = function(arr) {
 };
 
 /**
+ * 将TAG数组中所有拥有别名的TAG转换为单个TAG名 (第一个TAG名)。主要用于用户端TAG选择功能。
+ * @param {array} arr 用 getTagsForSelection 获取到的TAG数组
+ * @returns 
+ */
+Global._convertMultiNamedTagsToSingle = function(arr) {
+    var temp_arr = [];
+    arr.forEach((item)=>{
+        temp_arr.push(item.includes('/') ? item.split('/')[0] : item);
+    });
+    return temp_arr;
+}
+
+/**
+ * 将所有为别名的TAG转换成正名 (包含 _convertMultiNamedTagsToSingle 的功能)
+ * @param {array} arr 待转换的TAG数组
+ * @param {object} obj TAG原始对象 (JSON数据文件中的对象)
+ * @returns 将所有别名转换为正名的TAG数组
+ */
+Global._convertAllAliasesToMain = function(arr, obj) {
+    var mn_tags = this.getMultiNamedTags(obj);
+    var temp_arr = [];
+    arr.forEach((item)=>{
+        var parent_key = this._getKeyOfAlias(item);
+        if(item.includes('/')) {
+            temp_arr.push(item.split('/')[0]);
+        } else if(parent_key) {
+            temp_arr.push(mn_tags[parent_key][0]);
+        } else {
+            temp_arr.push(item);
+        }
+    });
+    return temp_arr;
+}
+
+Global._getKeyOfAlias = function(alias, obj) {
+    var mn_tags = this.getMultiNamedTags(obj);
+    var keys = Object.keys(mn_tags);
+    var target_key = '';
+    keys.forEach((item)=>{
+        var key = item;
+        var temp_arr = mn_tags[key];
+        if(temp_arr.contains(alias)) target_key = key;
+    });
+    return target_key;
+}
+
+/**
  * 获取所有有别名的TAG的对象。
- * @param {object} obj 传入的JSON数据文件中TAG结构JSON字符串
+ * @param {object} obj TAG原始对象 (JSON数据文件中的对象)
  * @returns 解析好的一个对象。
  */
 Global.getMultiNamedTags = function(obj) {
@@ -160,16 +240,4 @@ Global.getMultiNamedTags = function(obj) {
         Note that the max depth of each branch is 3.
     */
     return temp_obj;
-}
-
-/**
- * 将TAG数组中所有拥有别名的名字转换为单个名字。主要用于用户端TAG选择功能。
- * @param {array} arr 用 getTagsForSearch 获取到的TAG数组
- */
-Global._convertMultiNamedTagsToSingle = function(arr) {
-    var temp_arr = [];
-    arr.forEach((item)=>{
-        temp_arr.push(item.includes('/') ? item.split('/')[0] : item);
-    });
-    return temp_arr;
 }
