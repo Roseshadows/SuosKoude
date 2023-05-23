bc2b0fb8d6917f844a20b89cbacd81bdfa3986a0
Roseshadows - 离影玫 <99497945+Roseshadows@users.noreply.github.com>
2023-05-23 12:15:12 +0800

    Update search.js

--- web/search.js
+++ web/search.js
@@ -8,6 +8,7 @@ Global.search = function(){
 
 Global._onSearchStart = function() {
     this._searching = true;
+    this._hideSearchNotice();
     this._showVisualSearchSymbol();
     this._initializeDataForSearch();
     this._loadClientSearchConditions();
@@ -21,7 +22,6 @@ Global._onSearchEnd = function() {
     this._searching = false;
     this._hideVisualSearchSymbol();
     this._appendResult();
-    this._generatePagination();
 };
 
 Global._initializeDataForSearch = function() {
@@ -35,6 +35,8 @@ Global._initializeDataForSearch = function() {
     this._search_ending = '';
     this._search_is_yuhuang_only = '';
     this._search_is_yuhuangyu = '';
+    
+    this._search_target_url = [];
 };
 
 Global._loadClientSearchConditions = function(){
@@ -62,20 +64,43 @@ Global._loadClientSearchConditions = function(){
     this._search_is_yuhuangyu = iyhy == 'yes' ? true : false;
 };
 
+Global._hideSearchNotice = function() {
+    $('div.initial-result').css('display','none');
+    $('div.no-result').css('display','none');
+};
+
 Global._showVisualSearchSymbol = function() {
-    
+    $('div.searching-symbol').css('display','block');
 };
 
 Global._hideVisualSearchSymbol = function() {
-    
+    $('div.searching-symbol').css('display','none');
 };
 
 Global._appendResult = function() {
+    if(this._existsSearchResult()) {
+        var columns_in_one_page = 10;
+        var page_num = Math.floor(this._search_target_url / columns_in_one_page);
+        for(var i = 0; i < page_num; i++) {
+            var p = i + 1;
+            $('div.search-result-area').append('<div id="pg'+p+'"></div>');
+        }
+        this._search_target_url.forEach((item, index)=>{
+            var curPage = Math.floor(index / columns_in_one_page) + 1;
+            $('div#pg'+curPage).append('<div class="article-item"></div>')
+        })
+        this._generatePagination(page_num);
+    } else {
+        $('div.no-result').css('display','block');
+    }
+};
+
+Global._generatePagination = function(page_num) {
     
 };
 
-Global._generatePagination = function() {
-    
+Global._existsSearchResult = function(){
+    return JSON.stringify(this._search_target_url) != '[]';
 };
 
 Global.isSearching = function() {
