4ac0165cae67a05e78c6f772bb1bd033bb71c295
Roseshadows - 离影玫 <99497945+Roseshadows@users.noreply.github.com>
2023-06-01 17:01:12 +0800

    Update RSSD_SSIB_Addon_TouchScroll.js

--- RMMZ/RSSD_SSIB_Addon_TouchScroll.js
+++ RMMZ/RSSD_SSIB_Addon_TouchScroll.js
@@ -21,11 +21,46 @@ RSSD.SSIB_TS = {};
 (function() {
     var parameters = PluginManager.parameters(pluginName);
     if(Imported.RSSD_SimpleScrollableInfoBoard && Imported.TTK_SwipeMove) {
-        var _Input_update = Input.update;
-        Input.update = function() {
-            _Input_update.call(this);
-            
+        var _TouchInput_clear = TouchInput.clear;
+        TouchInput.clear = function() {
+            _TouchInput_clear.call(this);
+            this._storeForceY = 0;
+            this._boardMoveY = 0;
+            this._boardOriginY = 0;
         }
+        var _TouchInput_update = TouchInput.update;
+        TouchInput.update = function() {
+            _TouchInput_update.call(this);
+            if(this._boardMoveY > 0) {}
+        }
+        
+        var _TouchInput_onTrigger = TouchInput._onTrigger;
+        TouchInput._onTrigger = function(x, y) {
+            if (this.isMoved()) return;
+            _TouchInput_onTrigger.call(this, x, y);
+            this._boardMoveY = this.y;
+        };
+
+        var _TouchInput_onMove = TouchInput._onMove;
+        TouchInput._onMove = function(x, y) {
+            if ($.cancelPlayerMove)
+                $.denyMove = true;
+            this._lastCameraX = this.x;
+            this._lastCameraY = this.y;
+            _TouchInput_onMove.call(this, x, y);
+        };
+
+        var _TouchInput_onRelease = TouchInput._onRelease;
+        TouchInput._onRelease = function(x, y) {
+            _TouchInput_onRelease.call(this, x, y);
+            if (TTK.SwipeMove) {
+                this._cameraAccX = this._forceX;
+                this._cameraAccY = this._forceY;
+                this._storForceX = this._forceX / 48;
+                this._storForceY = this._forceY / 48;
+            }
+            if ($.denyMove) $.denyMove = false;
+        };
     } else {
         console.log('插件'+pluginName+'.js 未检测到前置插件。请去看看是缺少插件还是顺序不对!');
     }
